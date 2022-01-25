af45559db23a909424e2ffda73a8acfb9c32d781
BenkoAta <benkoata@gmail.com>
2022-01-25 07:38:38 +0100

    day01 feladat megoldása

---
+++ week14-lab-tasks/src/main/java/day01/OrderService.java
@@ -0,0 +1,51 @@
+package day01;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OrderService {
+    private List<Order> orders = new ArrayList<>();
+
+    public void saveOrder(Order order) {
+        orders.add(order);
+    }
+
+    public List<Order> findOrdersByStatus(String status) {
+        return orders.stream()
+                .filter(order -> order.getStatus().equals(status))
+                .collect(Collectors.toList());
+    }
+
+    public long countOrdersByStatus(String status) {
+        return orders.stream()
+                .filter(order -> order.getStatus().equals(status))
+                .count();
+    }
+
+    public List<Order> findOrdersBetweenDates(LocalDate from, LocalDate to) {
+        return orders.stream()
+                .filter(order -> order.getOrderDate().isAfter(from) && order.getOrderDate().isBefore(to))
+                .collect(Collectors.toList());
+    }
+
+    public boolean matchAnyOrderWithLessProductThan(int products) {
+        return orders.stream()
+                .anyMatch(order -> order.getProducts().size() < products);
+    }
+
+    public Order findOrderWithMostProducts() {
+        return orders.stream()
+                .sorted(Comparator.comparing(order -> order.getProducts().size(), Comparator.reverseOrder()))
+                .findFirst().orElseThrow(() -> new IllegalArgumentException("Empty list!"));
+    }
+
+    public List<Order> findOrdersWithProductByCategory(String category) {
+        return orders.stream()
+                .filter(order -> order.getProducts().stream()
+                        .anyMatch(product -> product.getCategory().equals(category)))
+                .collect(Collectors.toList());
+    }
+}
---
+++ week14-lab-tasks/src/test/java/day01/OrderServiceTest.java
@@ -0,0 +1,107 @@
+package day01;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    OrderService orderService;
+
+    @BeforeEach
+    void init() {
+        orderService = new OrderService();
+
+        Product p1 = new Product("Tv", "IT", 2000);
+        Product p2 = new Product("Laptop", "IT", 2400);
+        Product p3 = new Product("Phone", "IT", 400);
+        Product p4 = new Product("Lord of The Rings", "Book", 20);
+        Product p5 = new Product("Harry Potter Collection", "Book", 120);
+
+        Order o1 = new Order("pending", LocalDate.of(2021, 06, 07));
+        o1.addProduct(p1);
+        o1.addProduct(p2);
+        o1.addProduct(p5);
+
+        Order o2 = new Order("on delivery", LocalDate.of(2021, 06, 01));
+        o2.addProduct(p3);
+        o2.addProduct(p1);
+        o2.addProduct(p2);
+
+        Order o3 = new Order("pending", LocalDate.of(2021, 06, 07));
+        o3.addProduct(p1);
+        o3.addProduct(p2);
+        o3.addProduct(p5);
+
+        Order o4 = new Order("on delivery", LocalDate.of(2021, 06, 01));
+        o4.addProduct(p3);
+        o4.addProduct(p1);
+        o4.addProduct(p2);
+        o4.addProduct(p1);
+
+        Order o5 = new Order("pending", LocalDate.of(2021, 06, 07));
+        o5.addProduct(p1);
+        o5.addProduct(p2);
+        o5.addProduct(p5);
+
+        orderService.saveOrder(o1);
+        orderService.saveOrder(o2);
+        orderService.saveOrder(o3);
+        orderService.saveOrder(o4);
+        orderService.saveOrder(o5);
+    }
+
+    @Test
+    void findOrdersByStatusTest() {
+        List<Order> actual = orderService.findOrdersByStatus("pending");
+        assertEquals(3, actual.size());
+        assertEquals("pending", actual.get(0).getStatus());
+        
+    }
+
+    @Test
+    void countOrdersByStatus() {
+        long actual = orderService.countOrdersByStatus("pending");
+        assertEquals(3, actual);
+    }
+
+    @Test
+    void findOrdersBetweenDatesTest() {
+        List<Order> actual = orderService.findOrdersBetweenDates(LocalDate.of(2021, 6, 6), LocalDate.of(2021,6,8));
+        assertEquals("2021-06-07", actual.get(1).getOrderDate().toString());
+    }
+
+    @Test
+    void findOrdersBetweenDatesNotFoundTest() {
+        List<Order> actual = orderService.findOrdersBetweenDates(LocalDate.of(2021, 6, 7), LocalDate.of(2021,6,8));
+        assertEquals(0, actual.size());
+        actual = orderService.findOrdersBetweenDates(LocalDate.of(2021, 6, 6), LocalDate.of(2021,6,7));
+        assertEquals(0, actual.size());
+    }
+
+    @Test
+    void matchAnyOrderWithLessProductThan() {
+        assertTrue(orderService.matchAnyOrderWithLessProductThan(4));
+        assertFalse(orderService.matchAnyOrderWithLessProductThan(3));
+    }
+
+
+    @Test
+    void findOrderWithMostProductsTest() {
+        Order actual = orderService.findOrderWithMostProducts();
+        assertEquals(4, actual.getProducts().size());
+    }
+
+    @Test
+    void findOrdersWithProductByCategory() {
+        List<Order> actual = orderService.findOrdersWithProductByCategory("Book");
+        assertEquals(3, actual.size());
+        for (Order order: actual) {
+            assertTrue(order.getProducts().get(2).getCategory().equals("Book"));
+        }
+    }
+}
